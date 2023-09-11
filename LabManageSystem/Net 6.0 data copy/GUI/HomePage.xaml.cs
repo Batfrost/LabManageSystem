df6acc2aab7544ee799b97597a39d7c181c3d700
Batfrost <batmantis2003@gmail.com>
2023-09-10 18:31:22 -0600

    Got it to go int a MSIX File to install from, and then to run it as an exe, WINUI3 Might need to be installed to machine. Run the SpreadsheetUI to start.

---
+++ LabManageSystem/Net 6.0 data copy/GUI/HomePage.xaml.cs
@@ -0,0 +1,27 @@
+namespace SpreadsheetGUI;
+
+public partial class HomePage : ContentPage
+{
+	public SpreadsheetPage SprdSht;
+	public HomePage()
+	{
+		SprdSht = new SpreadsheetPage();
+		InitializeComponent();
+	}
+
+	async void GoToSpreadsheet(object sender, EventArgs e)
+	{
+		SprdSht = new SpreadsheetPage();
+		await Navigation.PushAsync(SprdSht);
+	}
+
+	void LoginUser(object sender, EventArgs e)
+	{
+		if (UIDEntry.Text.Length == 8)
+		{
+			SprdSht.LoginUser(int.Parse(UIDEntry.Text), "C:\\Users\\batma\\source\\repos\\LabManageSystem\\LabManageSystem\\GUI\\LogDemo.sprd");
+            UIDEntry.Text = "";
+        }
+		
+	}
+}
